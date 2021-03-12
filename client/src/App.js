6d102216f770e2dd9dd7fdb5a1c9e13f710c541d
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-12 22:40:19 +0200

    cover page
    cover page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo1.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Welcome in Our E - Jewelry Shop { /* <code>src/App.js</code> and save to reload. */ } <
+        /p> <
+        p > Let 's Start</p> <
+        a className = "App-link"
+        href = "/"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Home <
+        /a> < /
+        header > <
+        /div>
+    );
 }
 
 export default App;
