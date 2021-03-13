a76b416b94ed0204e868b32d8ad625951abf40eb
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-13 22:40:24 +0200

    -
    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
+import React from 'react';
 import logo from './logo1.png';
 import './App.css';
 
 function App() {
 
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
+    return ( 
+        <div className = "App" >
+        <header className = "App-header" >
+        <img src = { logo }
         className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Welcome in Our E - Jewelry Shop { /* <code>src/App.js</code> and save to reload. */ } <
-        /p> <
-        p > Let 's Start</p> <
-        a className = "App-link"
+        alt = "logo" />
+        <p>
+        Welcome in Our E - Jewelry Shop { /* <code>src/App.js</code> and save to reload. */ } 
+        </p>  
+        <p> Let 's Start</p>   
+        <a className = "App-link"
         href = "/"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Home <
-        /a> < /
-        header > <
-        /div>
+        Home 
+        </a>  
+        </header> 
+        </div>
     );
 }
 
