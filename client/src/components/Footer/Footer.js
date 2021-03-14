0bd6297e721e1f460b4d723adced2b7be67baf19
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-14 02:53:59 +0200

    Footer Component
    Footer Component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './Footer.module.css';
+
+function Footer() {
+    return (
+        <p className={style.FooterRow}>Copyright &copy; E-Jewerly </p>
+    );
+}
+export default Footer;
