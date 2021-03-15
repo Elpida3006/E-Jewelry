483ae8d5966f3036014f01800e902bc7aae82000
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-15 19:06:54 +0200

    About page Component

---
+++ client/src/page-components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './About.module.css';
+
+function About() {
+    return(
+<div className={style.About}>
+    <p>For information with us: </p>
+</div>
+    );
+}
+
+export default About;
