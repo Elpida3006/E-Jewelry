6987cb47c4331aade44ae96982503207b1b6c8a3
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-15 19:38:41 +0200

    router setup
    react-router-dom: global setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './tests/reportWebVitals';
-
+import {BrowserRouter as Router} from 'react-router-dom';
+//  Route, Link, Switch
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <Router> 
+       <App />
+    </Router>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
