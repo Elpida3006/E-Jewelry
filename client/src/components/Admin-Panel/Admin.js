3237b3055f2bb98ad90d013c40564e0b0559753a
Nadq Spasova <djuzepe3006@gmail.com>
2021-03-16 17:28:37 +0200

    Admin panel Components

---
+++ client/src/components/Admin-Panel/Admin.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import style from './Admin.module.css';
+import {Link} from 'react-router-dom';
+
+function Admin() {
+    return (
+        <div className={style.Admin}>
+
+
+             
+               
+      
+         <li className={style['Nav-AdminPanel']}> 
+         <li className={style['Nav-A']}>
+         Admin Panel
+         </li>
+         <li className={style['Nav-Li']}>
+            <Link className={style['Li']} to="/Create/Silver">Create Silver</Link>
+
+                </li>
+                <li className={style['Nav-Li']}>
+                <Link className={style['Li']} to="/Create/Gold">Create Gold</Link>
+
+                </li>
+                <li className={style['Nav-Li']}>
+                <Link className={style['Li']} to="/Create/Leather-Accessory">Create Leather Accessory</Link>
+
+                </li>
+         </li>
+      
+        </div>
+    );
+}
+
+
+
+export default Admin;
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -4,26 +4,33 @@ import logo from '../static/logo-home.jpg';
 import Menu from '../components/Menu/Menu';
 import Layout from '../components/Layout/Layout';
 // let  server = 'http://localhost:3002/api/';
-
+import Admin from '../components/Admin-Panel/Admin';
 function Home() {
 
 return (
-    <Layout>
-    <div className={style.Home} >
-
+<Layout>
+<div className={style.Home} >
+<div className={style.Static}>
     <div className={style.Menu}>
     <Menu/>
     </div>
       
     <div className={style.Image}>
         <img src = { logo } alt = "logo" />
+   
     </div>
+</div>
+
+    <div className={style.Admin}>
+    <Admin/>
+    </div>
+  
 {/* <Products/> */}
             {/* changed pages */}
     
 
-    </div>
-    </Layout>
+</div>
+</Layout>
 
 );
 
