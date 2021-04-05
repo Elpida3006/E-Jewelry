1667048e65d8f369e4aa3a20dcec731857aab385
Nadq Spasova <djuzepe3006@gmail.com>
2021-04-06 01:32:49 +0300

    implement inline css
    -

--- client/src/page-components/About/About.js
+++ client/src/page-components/About/About.js
@@ -2,16 +2,18 @@ import React from 'react';
 import style from './About.module.css';
 import Layout from '../../components/Layout/Layout';
 import {} from 'react-router-dom';
+import background from '../../static/gold-jewelry-background-vector-2075787.jpg';
 
 function About({match}) {
     return(
-        
+        <body style={{backgroundImage: `url(${background})` }} >       
 <Layout>
 <div className={style.About}>
     <h2>About Page</h2>
     <p>For information with us: </p>
 </div>
 </Layout>
+</body>
     );
 }
 
--- client/src/page-components/Contact/Contact.js
+++ client/src/page-components/Contact/Contact.js
@@ -3,11 +3,13 @@ import style from './Contact.module.css';
 import Layout from '../../components/Layout/Layout';
 // import {Link} from 'react-router-dom';
 import GoogleApiWrapper from '../../components/GoogleMap/GoogleMap';
+import background from '../../static/gold-jewelry-background-vector-2075787.jpg';
 
 
 function Contact({match}) {
     return(
-        
+        <body style={{backgroundImage: `url(${background})` }} >       
+   
 <Layout>
 <div className={style.Contact}>
     <h2>For Contact with us: </h2>
@@ -20,6 +22,7 @@ function Contact({match}) {
 
 </div>
 </Layout>
+</body>
     );
 }
 
